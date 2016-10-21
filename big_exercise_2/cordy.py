7bf1e7f987cf25e223f98fef9d6c597564965db7
lixin <80724547@qq.com>
2016-10-21 19:39:08 +0300

    test

--- big_exercise_2/cordy.py
+++ big_exercise_2/cordy.py
@@ -3,18 +3,15 @@
 #xin.li@helsinki.fi
 
 import pexpect
+from pexpect import pxssh
 
 def sendcat(action, catname, searchnode):
-	# 新建一个ssh客户端对象
-	myclient = ssh.SSHClient()
-	# 设置成默认自动接受密钥
-	myclient.set_missing_host_key_policy(ssh.AutoAddPolicy())
-	# 连接远程主机
-	myclient.connect("shell.cs.helsinki.fi", port=22, username="xgli", password="08358857675Lx")
-	# 在远程机执行shell命令
-	stdin, stdout, stderr = client.exec_command("ls -l")
-	# 读返回结果
-	print(stdout.read())
-	# 在远程机执行python脚本命令
-	stdin, stdout, stderr = client.exec_command("python test.py")
+	s = pxssh.pxssh()
+	s.login('ukko010.hpc.cs.helsinki.fi', 'xgli')
+	s.sendline('ls -l')
+	s.prompt()
+	print('hahah')
+
+sendcat(1, 2, 3)
+
 
