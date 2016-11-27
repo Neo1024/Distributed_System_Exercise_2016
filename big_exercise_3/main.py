7899a3194e8af4e2e05f6f2acbd119d7cf3b1d0f
lixin <80724547@qq.com>
2016-11-27 23:04:02 +0200

    fix

--- big_exercise_3/main.py
+++ big_exercise_3/main.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
 	#remotely running first method on ukko050 - 054 and save the results
 	for i in range(50, 55):
 		ukkonode = 'xgli@ukko0' + str(i) + '.hpc.cs.helsinki.fi '
-		comm = 'ssh ' + ukkonode + 'python3 first_method.py ' + str(base_num) + ' > id.txt'
+		comm = 'ssh ' + ukkonode + 'python3 /cs/home/xgli/Distributed_System_Exercise_2016/big_exercise_3/first_method.py ' \
+			+ str(base_num) + ' > id.txt'
 		os.system(comm)
 		f = open('id.txt', 'r')
 		results.append(f.read())
